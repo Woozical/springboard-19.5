889d6f5372284f3d76edadd03bae07839bb2dd12
Mark <mark@example.dev>
2021-07-28 21:11:38 -0700

    first pass on timer implementation

--- app.py
+++ app.py
@@ -17,21 +17,23 @@ def new_board():
     session['board']  = boggle_game.make_board()
     session['guessed'] = []
     session['score'] = 0
+    session['time_up'] = False
     return redirect('/')
 
-@app.route('/guess', methods=['GET'])
+@app.route('/guess', methods=['POST'])
 def receive_guess():
     """
     Endpoint for making a guess on the Boggle board in the current session.
-    Payload should be an object where {"guess": "some_string"}
+    Query should have parameter of 'guess' with the word to be guessed
     Response will be a JSON object where
     {
         "response" : string displaying guess outcome
         "score" : integer representing score value of guess
     }
+    As well as an updated session cookie
     """
-
-    guess = request.args.get('guess', '').lower()
+    data = request.get_json()['data']
+    guess = data['guess']
     outcome = handle_guess(guess)
     #update session cookie
     update_session(guess, outcome['score'])
@@ -41,7 +43,7 @@ def receive_guess():
 def handle_guess(word):
     """"
     Given a word, check if that word exists in the client's game board.
-    Will return a dict where {"response" : outcome}
+    Will return a dict where {"response" : outcome, "score": score change}
     
     {response: "ok"} --> guess is in game board
     {response: "not-word"} --> guess is not a recognized word
@@ -59,9 +61,7 @@ def handle_guess(word):
 
 
 def update_session(word, num):
-    """
-    Updates the session cookie to add word to list of guessed nums, and increases score by num.
-    """
+    """Updates the session cookie to add word to list of guessed nums, and increases score by num."""
     guessed = set(session.get('guessed', []))
     guessed.add(word)
     session['guessed'] = list(guessed)
--- static/guess.js
+++ static/guess.js
@@ -1,16 +1,36 @@
 const form = document.querySelector('#guess-form');
 const result = document.querySelector('#guess-result');
 const scoreDisplay = document.querySelector('#score');
+const timerDisplay = document.querySelector('#timer');
+let started = false;
+let globalTimer;
 
 async function formHandler(e){
     e.preventDefault();
     console.log(form.guess.value);
-    response = await axios.get('/guess', {
-        params: {guess: form.guess.value}
+    response = await axios.post('/guess', {
+        data: {guess: form.guess.value}
     });
 
     showGuessResult(response.data.response);
     updateScore(response.data.score)
+
+    if (!started){
+        globalTimer = setInterval(timeoutHandler, 1000);
+        started = true;
+    }
+
+}
+
+function timeoutHandler(){
+    time = +timerDisplay.innerText.split(" ")[1];
+    time -= 1;
+    timerDisplay.innerText = "Time: " + time;
+    if (time <= 0){
+        clearInterval(globalTimer);
+        const btn = document.querySelector('#guess-btn');
+        btn.setAttribute('disabled', '');
+    }
 }
 
 function showGuessResult(text){
