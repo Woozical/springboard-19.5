d5155c2c30bb26b1bf9554e98a317aebad34894f
Mark <mark@example.dev>
2021-07-28 18:22:07 -0700

    template rendering

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
 from boggle import Boggle
+from flask import Flask, session, render_template
 
 boggle_game = Boggle()
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "YOUR_KEY_HERE"
+
+@app.route('/')
+def home_view():
+    if not session.get('board'):
+        session['board']  = boggle_game.make_board()
+    return render_template('base.html')
